f92b85ae5c0cc7bacde57ccb2169578358151775
DesignWolf.io <McGeelovestosell@gmail.com>
2021-07-21 15:44:08 -0700

    create current page animation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-document
-  .querySelectorAll('a[href="' + document.URL + '"]')
-  .forEach(function (elem) {
+document.querySelectorAll("a").forEach(function (elem) {
+  if (elem.href.includes(window.location.pathname))
     elem.className += " current-Link";
-  });
+});
