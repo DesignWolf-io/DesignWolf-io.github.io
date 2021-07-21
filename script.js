6887f1f5c538d9e6db974c31b661f590e61ca624
DesignWolf.io <McGeelovestosell@gmail.com>
2021-07-21 16:00:53 -0700

    fix js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("a").forEach(function (elem) {
-    if (window.location.pathname == '')
+    if (window.location.pathname == '/')
       i=0;
     else if (elem.href.includes(window.location.pathname)){
       elem.className += " current-Link";
